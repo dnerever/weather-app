e7771a9473ac275706b6c907e8a8f90f6e4079d7
dnerever <dnerver@protonmail.com>
2026-05-10 20:37:44 -0600

    Added WeatherServices file to handle API connection to the location and weather endpoints

--- src/WeatherApp/Program.cs
+++ src/WeatherApp/Program.cs
@@ -1,6 +1,11 @@
 using WeatherApp.Components;
+using WeatherApp.Services;
+using WeatherApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient<WeatherService>();
+builder.Services.AddScoped<WeatherService>();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
